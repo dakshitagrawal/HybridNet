2a59af3511c039299d1d938961a3663e9db95c84
dakshitagrawal97 <dagrawal@cs.iitr.ac.in>
2018-09-11 15:35:43 +0530

    add metric meters for evaluation of model

---
+++ hybrid_net/MetricMeters.py
@@ -0,0 +1,66 @@
+from .DataTransformer import NO_LABEL
+
+class AverageMeterSet:
+    def __init__(self):
+        self.meters = {}
+
+    def __getitem__(self, key):
+        return self.meters[key]
+
+    def update(self, name, value, n=1):
+        if not name in self.meters:
+            self.meters[name] = AverageMeter()
+        self.meters[name].update(value, n)
+
+    def reset(self):
+        for meter in self.meters.values():
+            meter.reset()
+
+    def values(self, postfix=''):
+        return {name + postfix: meter.val for name, meter in self.meters.items()}
+
+    def averages(self, postfix='/avg'):
+        return {name + postfix: meter.avg for name, meter in self.meters.items()}
+
+    def sums(self, postfix='/sum'):
+        return {name + postfix: meter.sum for name, meter in self.meters.items()}
+
+    def counts(self, postfix='/count'):
+        return {name + postfix: meter.count for name, meter in self.meters.items()}
+
+
+class AverageMeter:
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __format__(self, format):
+        return "{self.val:{format}} ({self.avg:{format}})".format(self=self, format=format)
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    labeled_minibatch_size = max(target.ne(NO_LABEL).sum(), 1e-8)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / labeled_minibatch_size.float()))
+    return res
